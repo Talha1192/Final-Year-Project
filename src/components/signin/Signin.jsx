510bfbc2d29ced6c3598916d6c6b7bbe4b881bcf
Talha Maiken <talhamaiken92@gmail.com>
2021-06-16 21:26:57 +0500

    updatd signin

--- src/components/signin/Signin.jsx
+++ src/components/signin/Signin.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
+import React,{ useState} from 'react'
 import styles from './Signin.module.css'
 import avatar from '../../images/avatar.png'
 
 const Signin = () => {
-    return (
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    return (    
         <div className="sign in">
-          
-    <div className={styles.box}>
-    <img src={avatar} className={styles.avatar}></img>
-<h2>LOG IN</h2>
-<form>
-<label>ENTER E-MAIL </label>
 
-<input type="E-mail" placeholder="ENTER E-Mail"></input>
-<label>PASSWORD</label>
-<input type="PASWORD" placeholder="ENTER PASSWORD"></input>
-<input type="submit" value="LOG IN"></input>
-<a href="/register"> FORGET PASSWORD?</a>
-</form>
+            <div className={styles.box}>
+                <img src={avatar} className={styles.avatar}></img>
+                <h2>LOG IN</h2>
+                <form>
+                    <label>ENTER E-MAIL </label>
 
-</div>
+                    <input onChange={(e)=>setEmail(e.target.value)} value={email} type="E-mail" placeholder="ENTER E-Mail"></input>
+                    <label>PASSWORD</label>
+                    <input onChange={(e)=>setPassword(e.target.value)} value={password} type="PASWORD" placeholder="ENTER PASSWORD"></input>
+                    <input type="submit" value="LOG IN"></input>
+                    <a href="/register"> FORGET PASSWORD?</a>
+                </form>
+
+            </div>
         </div>
     )
 }
